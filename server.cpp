2ea612937c4a01cf6b59af3e542f7a872ab8d44d
Saswat Samal <saswat1091samal@gmail.com>
2026-04-08 04:56:55 +0530

    handle multiple requests

--- server.cpp
+++ server.cpp
@@ -38,6 +38,71 @@ static void do_something(int connfd)
 	write(connfd, wbuf, strlen(wbuf));
 }
 
+static int32_t read_full(int fd, char *buf, size_t n)
+{
+	while(n > 0){
+		ssize_t rv = read(fd, buf, n);
+		if(rv < 0) {
+			return -1; // error, or unexpected EOF
+		}
+		assert((size_t)rv <= n);
+		n -= (size_t)rv;
+		buf += rv;
+	}
+	return 0;
+}
+
+static int32_t write_all(int fd, char *buf, size_t n)
+{
+	while(n > 0) {
+		ssize_t rv = write(fd, buf, n);
+		if(rv < 0) {
+			return -1; // error
+		}
+		assert((size_t)rv <= n);
+		n -= (size_t)rv;
+		buf += rv;
+	}
+	return 0;
+}
+
+const size_t k_max_msg = 4096;
+static int32_t one_request(int connfd) 
+{
+	// 4 bytes header
+	char rbuf[4+k_max_msg];
+	errno = 0;
+	int32_t err = read_full(connfd, rbuf, 4);
+	if(err) {
+		msg(errno==0?"EOF": "read() error");
+		return err;
+	}
+
+	uint32_t len = 0;
+	memcpy(&len, rbuf, 4); // assume little endian
+	if(len > k_max_msg) {
+		msg("too long");
+		return -1;
+	}
+
+	// request body
+	err = read_full(connfd, &rbuf[4], len);
+	if(err) {
+		msg("read() error");
+		return err;
+	}
+
+	// do something
+	printf("client says: %.*s\n", len, &rbuf[4]);
+	// reply using the same protocol
+	const char reply[] = "world";
+	char wbuf[4+sizeof(reply)];
+	len = (uint32_t)strlen(reply);
+	memcpy(wbuf, &len, 4);
+	memcpy(&wbuf[4], reply, len);
+	return write_all(connfd, wbuf, 4+len);
+}
+
 int main()
 {
 	int fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -73,8 +138,13 @@ int main()
 		if(connfd < 0) {
 			continue; // error
 		}
-
-		do_something(connfd);
+		
+		while(true){
+			int32_t err = one_request(connfd);
+			if(err){
+				break;
+			}
+		}
 		close(connfd);
 	}
 
